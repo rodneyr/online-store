4cfee27df965de70d5c6f6cf592048bc568c11b0
rodneyr <rodneyr@comcast.net>
2013-11-13 18:51:07 -0500

    created includes

---
+++ _header.php
@@ -0,0 +1,8 @@
+<div id="header">
+	<h1 id="lolg">My Online Store</h1>
+    <ul>
+    	<li><a href="index.php">Home</a></li>
+        <li><a href="products.php">Products<a/></li>
+        <li><a href="add.php">Add a Product</a></li>
+    </ul>
+</div>
